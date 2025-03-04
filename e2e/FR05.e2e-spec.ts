dc227b5b96280fcda1ced49dc0486925cecb7083
Benjamin Zwittnig <benjamin.zwittnig@register.si>
2025-03-04 15:41:07 +0100

    remaining translations

---
+++ e2e/FR05.e2e-spec.ts
@@ -0,0 +1,37 @@
+const { test, expect } = require('@playwright/test');
+
+import { goToHome, setLang } from './utils/app.utils';
+
+test.describe('Zonemaster test FR05 - [Supports internationalization]', () => {
+  test.beforeEach(async ({ page }) => {
+    await goToHome(page);
+  });
+
+  const testSuite = [
+      { language: 'Danish', code: 'da', expected: 'Domænenavn' },
+      { language: 'English', code: 'en', expected: 'Domain name' },
+      { language: 'Spanish', code: 'es', expected: 'Nombre de dominio' },
+      { language: 'Finnish', code: 'fi', expected: 'Verkkotunnus' },
+      { language: 'French', code: 'fr', expected: 'Nom de domaine' },
+      { language: 'Norwegian', code: 'nb', expected: 'Domenenavn' },
+<<<<<<< HEAD
+      { language: 'Slovenščina', code: 'sl', expected: 'Domena' },
+=======
+      { language: 'Slovenian', code: 'sl', expected: 'Domena' },
+>>>>>>> a8f835d6111bbc4d0b3adf94e39c04bec0640061
+      { language: 'Swedish', code: 'sv', expected: 'Domännamn' },
+  ];
+
+  for (const { language, code, expected } of testSuite) {
+    test(`should have ${language} language option`, async ({ page }) => {
+      const langNavLink = page.locator(`select#languageSelection > option[lang="${code}"]`);
+      await expect(langNavLink).toHaveCount(1);
+      await expect(langNavLink).toHaveAttribute('lang', code);
+    })
+
+    test(`should switch to ${language}`, async ({ page }) => {
+      await setLang(page, code);
+      await expect(page.locator('label[for="domain-input"]')).toHaveText(expected);
+    })
+  }
+});
